a460b9b5b11ec6a8df4e1d72c29899e6e9c43c90
Ayush <gautamayush721@gmail.com>
2021-05-17 00:33:50 +0530

    SplashScreenAdded

--- app/src/main/java/com/hackslash/haaziri/splash/SplashActivity.java
+++ app/src/main/java/com/hackslash/haaziri/splash/SplashActivity.java
@@ -2,15 +2,29 @@ package com.hackslash.haaziri.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.hackslash.haaziri.MainActivity;
 import com.hackslash.haaziri.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Intent for start mainActivity
+                Intent splashIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_TIME);
+
     }
 }
