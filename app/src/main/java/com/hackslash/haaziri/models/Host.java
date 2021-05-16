bb895368bb270537e083bde1d08219ad25b64f10
SHIVAM3223 <shivambhushan653@gmail.com>
2021-05-16 07:54:19 -0700

    Competed

--- app/src/main/java/com/hackslash/haaziri/models/Host.java
+++ app/src/main/java/com/hackslash/haaziri/models/Host.java
@@ -1,4 +1,39 @@
 package com.hackslash.haaziri.models;
 
 public class Host {
+    public String name;
+    public String email;
+    public String phone;
+    public String uld;
+
+    public Host() {
+    }
+
+    public Host(String a,String b,String c){
+        a = "";
+        b = "";
+        c = "";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setName(String newName){ name = newName; }
+
+    public void setEmail(String newEmail){
+        email = newEmail;
+    }
+
+    public void setPhone(String newPhone){
+        phone = newPhone;
+    }
 }
