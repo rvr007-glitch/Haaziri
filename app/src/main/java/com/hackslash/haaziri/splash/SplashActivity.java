1475e2351cc822d782d9f7370a6ecf697a7639e5
ankit <ankitr.ug20.cse@nitp.ac.in>
2021-05-18 18:04:53 +0530

    splash screen design update

--- app/src/main/java/com/hackslash/haaziri/splash/SplashActivity.java
+++ app/src/main/java/com/hackslash/haaziri/splash/SplashActivity.java
@@ -3,6 +3,7 @@ package com.hackslash.haaziri.splash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.hackslash.haaziri.R;
 
@@ -12,5 +13,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
