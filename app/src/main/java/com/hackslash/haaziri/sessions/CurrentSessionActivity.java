f82a2226d0f1d653f5782f44b58ecc006b4f1bab
sonunayan48 <sonunayan48@gmail.com>
2021-06-24 20:35:55 +0530

    Minor changes and empty files created

---
+++ app/src/main/java/com/hackslash/haaziri/sessions/CurrentSessionActivity.java
@@ -0,0 +1,16 @@
+package com.hackslash.haaziri.sessions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackslash.haaziri.R;
+
+public class CurrentSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_session);
+    }
+}
