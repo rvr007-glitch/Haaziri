179303aa1f572f855dda4f72ba67530f6a539d27
Ayush <gautamayush721@gmail.com>
2021-07-02 23:31:03 +0530

    reset_password and adapter(java and xml) added

--- app/src/main/java/com/hackslash/haaziri/home/OwnedTeamsAdapter.java
+++ app/src/main/java/com/hackslash/haaziri/home/OwnedTeamsAdapter.java
@@ -1,4 +1,55 @@
 package com.hackslash.haaziri.home;
 
-public class OwnedTeamsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hackslash.haaziri.R;
+import com.hackslash.haaziri.models.Team;
+
+import java.util.List;
+
+public class OwnedTeamsAdapter extends RecyclerView.Adapter<OwnedTeamsAdapter.ViewHolder> {
+    private List<Team> ownedTeamList;
+
+    public void Adapter (List<Team>ownedTeamList){ this.ownedTeamList=ownedTeamList;}
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View ownedTeamView = LayoutInflater.from(parent.getContext()).inflate(R.layout.owned_team_card,parent,false);
+        return new ViewHolder(ownedTeamView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String ownedIcon = ownedTeamList.get(position).getTeamCode();
+
+        holder.setData(ownedIcon);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ownedTeamList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView ownedTeamsIcon;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ownedTeamsIcon = itemView.findViewById(R.id.ownedTeamIcon);
+        }
+
+        public void setData(String ownedIcon) {
+            ownedTeamsIcon.setText(ownedIcon);
+        }
+    }
 }
