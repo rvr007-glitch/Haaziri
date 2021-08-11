f8dcb66b28f1a2e965830584b754159153e9217e
sonunayan48 <sonunayan48@gmail.com>
2021-08-11 23:07:06 +0530

    Next task essentials done

--- app/src/main/java/com/hackslash/haaziri/teamhome/RecentSessionsAdapter.java
+++ app/src/main/java/com/hackslash/haaziri/teamhome/RecentSessionsAdapter.java
@@ -1,4 +1,49 @@
 package com.hackslash.haaziri.teamhome;
 
-public class RecentSessionsAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hackslash.haaziri.models.Session;
+
+import java.util.ArrayList;
+
+public class RecentSessionsAdapter extends RecyclerView.Adapter<RecentSessionsAdapter.RecentHolder> {
+
+    private final Context mContext;
+    //Arraylist that would hold recent session objects
+    private ArrayList<Session> recentSessions;
+
+    public RecentSessionsAdapter(ArrayList<Session> recentSessions, Context mContext) {
+        this.recentSessions = recentSessions;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public RecentSessionsAdapter.RecentHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //TODO: Implement function as per requirement
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecentSessionsAdapter.RecentHolder holder, int position) {
+        //TODO: Implement function as per requirement
+    }
+
+    @Override
+    public int getItemCount() {
+        return recentSessions.size();
+    }
+
+    public class RecentHolder extends RecyclerView.ViewHolder {
+        //TODO: Write code for the viewholder as per requirement as per requirement
+        public RecentHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
